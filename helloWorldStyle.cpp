cbd214e47ce0612444af217401c79f5333e45088
Nathan Araujo <nathan.araujo@stu.ocsb.ca>
2022-09-15 00:23:04 +0000

    Added Hello world with style in C++

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Nathan Araujo All rights reserved.
+// .
+// Created by: Nathan Araujo
+// Date: Sept. 14, 2022
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+    std::cout << "I am learning how to code in C++" << std::endl;
+}
